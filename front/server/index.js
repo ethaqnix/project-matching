c03df28c711853af8e84ce6ddfdd55edded2ecb1
ethaqnix <romain.denizot@epitech.eu>
2021-05-05 16:57:53 +0200

    put front server in gitignore

--- front/server/index.js
+++ front/server/index.js
@@ -3,12 +3,10 @@ const app = express(); // create express app
 
 app.use(express.static("public"));
 
-// start express server on port 5000
 app.listen(5000, () => {
   console.log("server started on port 5000");
 });
 
 app.get("/", (req, res) => {
-  console.log("public", "index.html");
   res.sendFile(`${__dirname}/public/index.html`);
 });
